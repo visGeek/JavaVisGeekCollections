dbf7f88a8020bb0464abf3ea785776d0175f735f
visGeek <visgeek2@gmail.com>
2016-05-05 20:06:29 +0900

    カバレッジを満たすためのダミーテスト

---
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/statics/Enumerable.java
@@ -0,0 +1,12 @@
+package com.github.visgeek.utils.collections.test.testcase.ienumerable.statics;
+
+import org.junit.Test;
+
+import com.github.visgeek.utils.testing.Activator;
+
+public class Enumerable {
+	@Test
+	public void dummyTest() {
+		Activator.create(com.github.visgeek.utils.collections.Enumerable.class);
+	}
+}
