51d9d2b222a541f68e659882ad3e0803db102dd8
visGeek <visgeek2@gmail.com>
2016-05-06 02:38:24 +0900

    記述する必要がないコードをコメントアウトした。

--- src/src/main/java/com/github/visgeek/utils/collections/ILookup.java
+++ src/src/main/java/com/github/visgeek/utils/collections/ILookup.java
@@ -1,10 +1,11 @@
 package com.github.visgeek.utils.collections;
 
 public interface ILookup<TKey, TElement> extends IEnumerable<IGrouping<TKey, TElement>> {
-	@Override
-	default int count() {
-		return IEnumerable.super.count();
-	}
+	// 記述する必要がないのでコメントアウトしてある。
+	// @Override
+	// default int count() {
+	// return IEnumerable.super.count();
+	// }
 
 	IEnumerable<TElement> get(TKey key);
 }
