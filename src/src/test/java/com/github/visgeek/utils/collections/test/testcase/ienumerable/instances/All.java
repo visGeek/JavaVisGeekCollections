7089c2e9852e41afe52c00058046f2638627c6fd
visGeek <visgeek2@gmail.com>
2016-05-05 22:26:54 +0900

    テスト内容変更

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/All.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/All.java
@@ -8,13 +8,13 @@ import com.github.visgeek.utils.collections.Enumerable;
 public class All {
 	@Test
 	public void test_all01() {
-		boolean actual = Enumerable.of(1, 2, 3).all(n -> 1 <= n && n <= 3);
+		boolean actual = Enumerable.of(1, 2, 3).all(n -> n <= 3);
 		Assert.assertTrue(actual);
 	}
 
 	@Test
 	public void test_all02() {
-		boolean actual = Enumerable.of(1, 2, 3).all(n -> 1 <= n && n <= 2);
+		boolean actual = Enumerable.of(1, 2, 3, 4).all(n -> n <= 3);
 		Assert.assertFalse(actual);
 	}
 }
