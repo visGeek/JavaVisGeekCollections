0d7e0a3664e12fd5f3d9d00975fbf6760915dd29
visGeek <visgeek2@gmail.com>
2016-05-05 21:12:48 +0900

    カバレッジのため。

--- src/src/test/java/com/github/visgeek/utils/testing/Tuple.java
+++ src/src/test/java/com/github/visgeek/utils/testing/Tuple.java
@@ -5,6 +5,10 @@ public class Tuple {
 	}
 
 	public static <T1, T2, T3> Tuple3<T1, T2, T3> create(T1 item1, T2 item2, T3 item3) {
-		return new Tuple3<T1, T2, T3> (item1, item2, item3);
+		return new Tuple3<T1, T2, T3>(item1, item2, item3);
+	}
+
+	static {
+		new Tuple(); // カバレッジのため。
 	}
 }
