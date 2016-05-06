b3669651bc75f5235b9163b6502d2fec82032029
visGeek <visgeek2@gmail.com>
2016-05-07 02:41:17 +0900

    Aggregate のテスト

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/Aggregate.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/Aggregate.java
@@ -4,37 +4,40 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.github.visgeek.utils.collections.Enumerable;
+import com.github.visgeek.utils.testing.Assert2;
 
 public class Aggregate {
 	@Test
-	public void test_aggregate01_01() {
+	public void test01_01() {
+		Assert2.exceptionThrown(NullPointerException.class, () -> Enumerable.of(1, 2, 3).aggregate(null));
+	}
+
+	@Test
+	public void test01_02() {
 		int actual = Enumerable.of(1, 2, 3).aggregate((n1, n2) -> n1 + n2);
 		Assert.assertEquals(6, actual);
 	}
 
 	@Test
-	public void test_aggregate01_02() {
-		try {
-			Enumerable.empty(Integer.class).aggregate((n1, n2) -> n1 + n2);
-			Assert.fail();
-		} catch (Exception e) {
-			//
-		}
+	public void test01_03() {
+		Assert2.exceptionThrown(IllegalStateException.class, () -> Enumerable.empty(Integer.class).aggregate((n1, n2) -> n1 + n2));
 	}
 
 	@Test
-	public void test_aggregate02_01() {
+	public void test02_01() {
 		String actual =
-				Enumerable.of(1, 2, 3)
+				Enumerable
+						.of(1, 2, 3)
 						.aggregate(">", (n1, n2) -> n1 + n2);
 
 		Assert.assertEquals(actual, ">123");
 	}
 
 	@Test
-	public void test_aggregate02_02() {
+	public void test02_02() {
 		String actual =
-				Enumerable.of(1, 2, 3)
+				Enumerable
+						.of(1, 2, 3)
 						.aggregate(">", (n1, n2) -> n1 + n2, n -> String.format("[%s]", n));
 
 		Assert.assertEquals(actual, "[>123]");
