08ce4e38c7e3f0f69f2928e42cea7d9ab5d76a91
visGeek <visgeek2@gmail.com>
2016-05-09 07:01:43 +0900

    elementAtOrDefault テスト

---
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/ilistenumerable/ElementAtOrDefault.java
@@ -0,0 +1,82 @@
+package com.github.visgeek.utils.collections.test.testcase.ienumerable.instances.ilistenumerable;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.visgeek.utils.collections.Enumerable;
+
+public class ElementAtOrDefault {
+	@Test
+	public void test_elementAtOrDefault01_01() {
+		int actual = Enumerable.of(1, 2, 3).elementAtOrDefault(1);
+		Assert.assertEquals(2, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault01_02() {
+		Integer actual = Enumerable.of(1, 2, 3).elementAtOrDefault(-1);
+		Assert.assertEquals(null, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault01_03() {
+		Integer actual = Enumerable.of(1, 2, 3).elementAtOrDefault(3);
+		Assert.assertEquals(null, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault02_01() {
+		int actual = Enumerable.range(1, 3).elementAtOrDefault(1);
+		Assert.assertEquals(2, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault02_02() {
+		Integer actual = Enumerable.range(1, 3).elementAtOrDefault(-1);
+		Assert.assertEquals(null, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault02_03() {
+		Integer actual = Enumerable.range(1, 3).elementAtOrDefault(3);
+		Assert.assertEquals(null, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault03_01() {
+		int actual = Enumerable.of(1, 2, 3).elementAtOrDefault(1, 4);
+		Assert.assertEquals(2, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault03_02() {
+		int actual = Enumerable.of(Arrays.asList(1, 2, 3)).elementAtOrDefault(-1, 4);
+		Assert.assertEquals(4, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault03_03() {
+		int actual = Enumerable.of(Arrays.asList(1, 2, 3)).elementAtOrDefault(3, 4);
+		Assert.assertEquals(4, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault04_01() {
+		int actual = Enumerable.range(1, 3).elementAtOrDefault(1, 4);
+		Assert.assertEquals(2, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault04_02() {
+		int actual = Enumerable.range(1, 3).elementAtOrDefault(-1, 4);
+		Assert.assertEquals(4, actual);
+	}
+
+	@Test
+	public void test_elementAtOrDefault04_03() {
+		int actual = Enumerable.range(1, 3).elementAtOrDefault(3, 4);
+		Assert.assertEquals(4, actual);
+	}
+}
