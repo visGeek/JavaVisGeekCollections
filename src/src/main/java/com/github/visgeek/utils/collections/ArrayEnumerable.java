200fca74a61a8240c9e5b8e4eae2306069a1a06e
visGeek <visgeek2@gmail.com>
2016-05-08 10:06:50 +0900

    git 取り扱いミスの修復

--- src/src/main/java/com/github/visgeek/utils/collections/ArrayEnumerable.java
+++ src/src/main/java/com/github/visgeek/utils/collections/ArrayEnumerable.java
@@ -8,14 +8,3 @@ abstract class ArrayEnumerable<T> implements IListEnumerable<T> {
 		return Enumerable.range(0, this.count()).select(idx -> this.elementAt(idx)).iterator();
 	}
 }
-=======
-package com.github.visgeek.utils.collections;
-
-import java.util.Iterator;
-
-abstract class ArrayEnumerable<T> implements IListEnumerable<T> {
-	@Override
-	public final Iterator<T> iterator() {
-		return Enumerable.range(0, this.count()).select(idx -> this.elementAt(idx)).iterator();
-	}
-}
--- src/src/main/java/com/github/visgeek/utils/collections/IListEnumerable.java
+++ src/src/main/java/com/github/visgeek/utils/collections/IListEnumerable.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.github.visgeek.utils.collections;
 
 import com.github.visgeek.utils.Func0;
@@ -64,70 +63,3 @@ interface IListEnumerable<T> extends ICollectionEnumerable<T> {
 		}
 	}
 }
-=======
-package com.github.visgeek.utils.collections;
-
-import com.github.visgeek.utils.Func0;
-
-interface IListEnumerable<T> extends ICollectionEnumerable<T> {
-	@Override
-	T elementAt(int index);
-
-	@Override
-	default T elementAtOrDefault(int index, Func0<T> defaultValue) {
-		if (0 <= index && index < this.count()) {
-			return this.elementAt(index);
-		} else {
-			return defaultValue.func();
-		}
-	}
-
-	@Override
-	default T first() {
-		if (this.any()) {
-			return this.elementAt(0);
-		} else {
-			throw Errors.empty();
-		}
-	}
-
-	@Override
-	default T firstOrDefault(T defaultValue) {
-		return this.any() ? this.first() : defaultValue;
-	}
-
-	@Override
-	default T last() {
-		if (this.any()) {
-			return this.elementAt(this.count() - 1);
-		} else {
-			throw Errors.empty();
-		}
-	}
-
-	@Override
-	default T lastOrDefault(T defaultValue) {
-		T value = defaultValue;
-
-		if (this.any()) {
-			value = this.last();
-		}
-
-		return value;
-	}
-
-	@Override
-	default IEnumerable<T> reverse() {
-		return Enumerable.range(0, this.count()).select(idx -> this.elementAt(this.count() - idx - 1));
-	}
-
-	@Override
-	default T single() {
-		if (this.count() == 1) {
-			return this.first();
-		} else {
-			throw Errors.moreThanOneElement();
-		}
-	}
-}
->>>>>>> 61c0c141cd310cfdeb5e0372bb1c84b141ba062f
