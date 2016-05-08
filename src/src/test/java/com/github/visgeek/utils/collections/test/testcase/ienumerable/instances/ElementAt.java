1308971bc20746ed7a22e3a59fc280d76282a602
visGeek <visgeek2@gmail.com>
2016-05-09 04:24:24 +0900

    IEnumerable.ElementAt() テスト

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/ElementAt.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/ElementAt.java
@@ -1,43 +1,32 @@
 package com.github.visgeek.utils.collections.test.testcase.ienumerable.instances;
 
-import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.github.visgeek.utils.Action0;
 import com.github.visgeek.utils.collections.Enumerable;
+import com.github.visgeek.utils.collections.IEnumerable;
 import com.github.visgeek.utils.testing.Assert2;
 
 public class ElementAt {
 	@Test
-	public void test_elementAt01_01() {
-		int actual = Enumerable.of(Arrays.asList(1, 2, 3)).elementAt(1);
+	public void testSuccess() {
+		IEnumerable<Integer> source = Enumerable.forTo(1, 3);
+		int actual = source.elementAt(1);
 		Assert.assertEquals(2, actual);
 	}
 
 	@Test
-	public void test_elementAt01_02() {
-		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, () -> Enumerable.of(Arrays.asList(1, 2, 3)).elementAt(-1));
+	public void testIndexOutOfRange01() {
+		IEnumerable<Integer> source = Enumerable.forTo(1, 3);
+		Action0 action = () -> source.elementAt(-1);
+		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, action);
 	}
 
 	@Test
-	public void test_elementAt01_03() {
-		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, () -> Enumerable.of(Arrays.asList(1, 2, 3)).elementAt(3));
-	}
-
-	@Test
-	public void test_elementAt02_01() {
-		int actual = Enumerable.range(1, 3).elementAt(1);
-		Assert.assertEquals(2, actual);
-	}
-
-	@Test
-	public void test_elementAt02_02() {
-		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, () -> Enumerable.range(1, 3).elementAt(-1));
-	}
-
-	@Test
-	public void test_elementAt02_03() {
-		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, () -> Enumerable.range(1, 3).elementAt(3));
+	public void testIndexOutOfRange022() {
+		IEnumerable<Integer> source = Enumerable.forTo(1, 3);
+		Action0 action = () -> source.elementAt(4);
+		Assert2.assertExceptionThrown(IndexOutOfBoundsException.class, action);
 	}
 }
