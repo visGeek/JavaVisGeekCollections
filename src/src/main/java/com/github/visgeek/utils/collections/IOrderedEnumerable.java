4903d1693d2748116753fa1b94a5820ebb71e6f8
visGeek <visgeek2@gmail.com>
2016-05-10 15:24:17 +0900

    IOrderedEnumerable.thenBy() 系メソッド

--- src/src/main/java/com/github/visgeek/utils/collections/IOrderedEnumerable.java
+++ src/src/main/java/com/github/visgeek/utils/collections/IOrderedEnumerable.java
@@ -2,6 +2,7 @@ package com.github.visgeek.utils.collections;
 
 import java.util.Comparator;
 
+import com.github.visgeek.utils.ComparatorUtils;
 import com.github.visgeek.utils.Func1;
 
 public interface IOrderedEnumerable<T> extends IEnumerable<T> {
@@ -9,55 +10,51 @@ public interface IOrderedEnumerable<T> extends IEnumerable<T> {
 
 	default <TKey extends Comparable<TKey>> IOrderedEnumerable<T> thenBy(Func1<? super T, TKey> keySelector) {
 		Errors.throwIfNull(keySelector, "keySelector");
-		return this.thenBy(Comparator.comparing(keySelector::func));
+
+		Comparator<? super T> comparator = Comparator.comparing(keySelector::func);
+		comparator = Comparator.nullsFirst(comparator); // null 優先
+
+		return this.thenBy(comparator);
 	}
 
 	default <TKey> IOrderedEnumerable<T> thenBy(Func1<? super T, TKey> keySelector, Comparator<? super TKey> comparator) {
 		Errors.throwIfNull(keySelector, "keySelector");
-		Errors.throwIfNull(comparator, "comparator");
+
+		if (comparator == null) {
+			comparator = ComparatorUtils.getDefault();
+		}
+
 		return this.thenBy(Comparator.comparing(keySelector::func, comparator));
 	}
 
 	IOrderedEnumerable<T> thenBy(Comparator<? super T> comarator);
 
 	default IOrderedEnumerable<T> thenByDefaultKey() {
-		Comparator<T> comparator =
-				new Comparator<T>() {
-					@Override
-					public int compare(T arg0, T arg1) {
-						@SuppressWarnings("unchecked")
-						Comparable<T> a = (Comparable<T>) arg0;
-
-						return a.compareTo(arg1);
-					}
-				};
-
+		Comparator<T> comparator = ComparatorUtils.getDefault();
 		return this.thenBy(comparator);
 	}
 
 	default IOrderedEnumerable<T> thenByDefaultKeyDescending() {
-		Comparator<T> comparator =
-				new Comparator<T>() {
-					@Override
-					public int compare(T arg0, T arg1) {
-						@SuppressWarnings("unchecked")
-						Comparable<T> a = (Comparable<T>) arg0;
-
-						return a.compareTo(arg1);
-					}
-				};
-
+		Comparator<T> comparator = ComparatorUtils.getDefault();
 		return this.thenByDescending(comparator);
 	}
 
 	default <TKey extends Comparable<TKey>> IOrderedEnumerable<T> thenByDescending(Func1<? super T, TKey> keySelector) {
 		Errors.throwIfNull(keySelector, "keySelector");
-		return this.thenByDescending(Comparator.comparing(keySelector::func));
+
+		Comparator<? super T> comparator = Comparator.comparing(keySelector::func);
+		comparator = Comparator.nullsFirst(comparator); // null 優先
+
+		return this.thenByDescending(comparator);
 	}
 
 	default <TKey> IOrderedEnumerable<T> thenByDescending(Func1<? super T, TKey> keySelector, Comparator<? super TKey> comparator) {
 		Errors.throwIfNull(keySelector, "keySelector");
-		Errors.throwIfNull(comparator, "comparator");
+
+		if (comparator == null) {
+			comparator = ComparatorUtils.getDefault();
+		}
+
 		return this.thenByDescending(Comparator.comparing(keySelector::func, comparator));
 	}
 
