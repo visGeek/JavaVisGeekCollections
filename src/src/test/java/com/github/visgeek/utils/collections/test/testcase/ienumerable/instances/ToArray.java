0ab6eb3bfd4eaa39b84fad204afcbc05f6fb854d
visGeek <visgeek2@gmail.com>
2016-05-11 07:17:07 +0900

    IEnumerable.toArray()

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/ToArray.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/ienumerable/instances/ToArray.java
@@ -3,30 +3,28 @@ package com.github.visgeek.utils.collections.test.testcase.ienumerable.instances
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.github.visgeek.utils.Action0;
 import com.github.visgeek.utils.collections.Enumerable;
+import com.github.visgeek.utils.collections.IEnumerable;
+import com.github.visgeek.utils.testing.Assert2;
 
 public class ToArray {
 	@Test
-	public void test_toArray() {
-		Integer[] actual = Enumerable.of(1, 2, 3).toArray(Integer.class);
-		Assert.assertArrayEquals(actual, new Integer[] { 1, 2, 3 });
+	public void argIsNull() {
+		IEnumerable<Integer> source = () -> Enumerable.empty(Integer.class).iterator();
+		Class<Integer> elementClass = null;
+		Action0 action = () -> source.toArray(elementClass);
+
+		Assert2.assertNullPointerExceptionThrown("elementClass", action);
 	}
 
 	@Test
-	public void test_toDoubleArray() {
-		double[] actual = Enumerable.of(1.0, 2.0, 3.0).toDoubleArray();
-		Assert.assertArrayEquals(actual, new double[] { 1.0, 2.0, 3.0 }, 0);
-	}
+	public void success() {
+		IEnumerable<Integer> source = () -> Enumerable.of(1, 2, 3).iterator();
+		Class<Integer> elementClass = Integer.class;
+		Integer[] expected = new Integer[] { 1, 2, 3 };
 
-	@Test
-	public void test_toIntArray() {
-		int[] actual = Enumerable.of(1, 2, 3).toIntArray();
-		Assert.assertArrayEquals(actual, new int[] { 1, 2, 3 });
-	}
-
-	@Test
-	public void test_toLongArray() {
-		long[] actual = Enumerable.of(1L, 2L, 3L).toLongArray();
-		Assert.assertArrayEquals(actual, new long[] { 1L, 2L, 3L });
+		Integer[] actual = source.toArray(elementClass);
+		Assert.assertArrayEquals(actual, expected);
 	}
 }
