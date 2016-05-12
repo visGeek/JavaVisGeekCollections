5be3291923fc46963f0d4e850099fb86b290186c
visGeek <visgeek2@gmail.com>
2016-05-12 17:16:28 +0900

    EnumerableList

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/list/EnumerableListTest.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/list/EnumerableListTest.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,11 +15,7 @@ import com.github.visgeek.utils.testing.Assert2;
 public class EnumerableListTest {
 	@Test
 	public void ctorNoArgs() {
-		EnumerableList<Integer> actual = new EnumerableList<>();
-		actual.add(1);
-		actual.add(2);
-		actual.add(3);
-
+		EnumerableList<Integer> actual = new EnumerableList<>(1, 2, 3);
 		Assert2.assertSequanceEquals(actual, 1, 2, 3);
 	}
 
@@ -65,65 +60,6 @@ public class EnumerableListTest {
 	}
 
 	@Test
-	public void addAll01() {
-		Collection<Integer> valuesCollectionCollection = Arrays.asList(1, 2, 3);
-		Iterable<Integer> valuesIterableCollection = valuesCollectionCollection;
-		Iterable<Integer> valuesIterableIterable = () -> valuesCollectionCollection.iterator();
-
-		EnumerableList<Integer> list1 = new EnumerableList<>();
-		EnumerableList<Integer> list2 = new EnumerableList<>();
-		EnumerableList<Integer> list3 = new EnumerableList<>();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.addAll(valuesCollectionCollection);
-			boolean result2 = list2.addAll(valuesIterableCollection);
-			boolean result3 = list3.addAll(valuesIterableIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert.assertEquals(message, result1, result3);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
-	}
-
-	@Test
-	public void addAll02() {
-		Collection<Integer> valuesCollectionCollection = Arrays.asList(2, 3, 4);
-		Iterable<Integer> valuesIterableCollection = valuesCollectionCollection;
-		Iterable<Integer> valuesIterableIterable = () -> valuesCollectionCollection.iterator();
-
-		EnumerableList<Integer> list1 = new EnumerableList<>(1, 5);
-		EnumerableList<Integer> list2 = list1.clone();
-		EnumerableList<Integer> list3 = list1.clone();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.addAll(2, valuesCollectionCollection);
-			boolean result2 = list2.addAll(2, valuesIterableCollection);
-			boolean result3 = list3.addAll(2, valuesIterableIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert.assertEquals(message, result1, result3);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
-	}
-
-	@Test
-	public void any() {
-		EnumerableList<Integer> list = new EnumerableList<>();
-		Assert.assertFalse(list.any());
-
-		list.add(1);
-		Assert.assertTrue(list.any());
-	}
-
-	@Test
 	public void clone01() {
 		EnumerableList<Integer> list1 = new EnumerableList<>();
 		list1.add(1);
@@ -142,130 +78,69 @@ public class EnumerableListTest {
 	@Test
 	@SuppressWarnings("deprecation")
 	public void contains() {
-		EnumerableList<Integer> list = new EnumerableList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3);
 		Assert.assertTrue(list.contains(1));
 		Assert.assertFalse(list.contains(4));
-		Assert.assertTrue(list.containsValue(1));
-		Assert.assertFalse(list.containsValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void indexOf() {
-		EnumerableList<Integer> list = new EnumerableList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(2);
-
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3, 2);
 		Assert.assertEquals(1, list.indexOf(2));
 		Assert.assertEquals(-1, list.indexOf(4));
-		Assert.assertEquals(list.indexOf(2), list.indexOfValue(2));
-		Assert.assertEquals(list.indexOf(4), list.indexOfValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void lastIndexOf() {
-		EnumerableList<Integer> list = new EnumerableList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(2);
-
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3, 2);
 		Assert.assertEquals(3, list.lastIndexOf(2));
 		Assert.assertEquals(-1, list.lastIndexOf(4));
-		Assert.assertEquals(list.lastIndexOf(2), list.lastIndexOfValue(2));
-		Assert.assertEquals(list.lastIndexOf(4), list.lastIndexOfValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
-	public void removeIndex_removeAt() {
-		EnumerableList<Integer> list1 = new EnumerableList<>();
-		list1.add(1);
-		list1.add(2);
-		list1.add(3);
+	public void removeIndex() {
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3);
 
-		EnumerableList<Integer> list2 = list1.clone();
-
-		int index = 1;
-		Integer result1 = list1.remove(index);
-		Integer result2 = list2.removeAt(index);
-
-		Assert.assertEquals(Integer.valueOf(2), result1);
-		Assert.assertEquals(result1, result2);
-		Assert2.assertSequanceEquals(list1, list2);
-	}
-
-	@Test
-	@SuppressWarnings("deprecation")
-	public void removeObject_removeValue() {
-		int target = 2;
-
-		EnumerableList<Integer> list1 = new EnumerableList<>();
-		list1.add(target - 1);
-		list1.add(target);
-		list1.add(target + 1);
-		list1.add(target);
-
-		EnumerableList<Integer> list2 = list1.clone();
-
-		// target をすべて消して更にもう一回試みる。
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.remove(Integer.valueOf(target));
-			boolean result2 = list2.removeValue(target);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert2.assertSequanceEquals(message, list1, list2);
+		{
+			int index = 1;
+			int expected = 2;
+			Integer actual = list.remove(index);
+			Assert.assertEquals(expected, (int) actual);
 		}
+
+		{
+			int index = 0;
+			int expected = 1;
+			Integer actual = list.remove(index);
+			Assert.assertEquals(expected, (int) actual);
+		}
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void removeObject() {
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3);
+		Integer target = 2;
+		boolean actual = list.remove(target);
+		Assert.assertTrue(actual);
+		Assert2.assertSequanceEquals(list, 1, 3);
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void removeAllCollection() {
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3, 2, 4);
 		Collection<Integer> targetCollection = Arrays.asList(2, 3);
-		Iterable<Integer> targetIterable = () -> targetCollection.iterator();
-
-		EnumerableList<Integer> list1 = new EnumerableList<>();
-		list1.add(1);
-		list1.add(2);
-		list1.add(3);
-		list1.add(2);
-		list1.add(4);
-
-		EnumerableList<Integer> list2 = list1.clone();
-		EnumerableList<Integer> list3 = list1.clone();
-
-		// target をすべて消して更にもう一回試みる。
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.removeAll(targetCollection);
-			boolean result2 = list2.removeAllValues(targetCollection);
-			boolean result3 = list3.removeAllValues(targetIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result2, result1);
-			Assert.assertEquals(message, result3, result1);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
+		list.removeAll(targetCollection);
+		Assert2.assertSequanceEquals(list, 1, 4);
 	}
 
 	@Test
 	public void toString01() {
-		EnumerableList<Integer> list = new EnumerableList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-
+		EnumerableList<Integer> list = new EnumerableList<>(1, 2, 3);
 		Assert.assertEquals("size:3", list.toString());
 	}
 
@@ -287,7 +162,7 @@ public class EnumerableListTest {
 		Assert.assertTrue(list.isEmpty());
 	}
 
-	static int getCapacity(List<?> list) {
+	static int getCapacity(ArrayList<?> list) {
 		try {
 			// リフレクションで無理矢理キャパシティを取得する。
 			Field field = ArrayList.class.getDeclaredField("elementData");
