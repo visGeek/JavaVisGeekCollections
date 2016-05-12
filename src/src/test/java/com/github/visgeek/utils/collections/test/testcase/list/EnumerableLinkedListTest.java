474eb7e41854bc514fc42974fa10c6867dbcb8c0
visGeek <visgeek2@gmail.com>
2016-05-12 17:35:29 +0900

    EnumerableLinkedList

--- src/src/test/java/com/github/visgeek/utils/collections/test/testcase/list/EnumerableLinkedListTest.java
+++ src/src/test/java/com/github/visgeek/utils/collections/test/testcase/list/EnumerableLinkedListTest.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.github.visgeek.utils.Func2;
 import com.github.visgeek.utils.collections.EnumerableLinkedList;
 import com.github.visgeek.utils.collections.IReadOnlyList;
 import com.github.visgeek.utils.testing.Assert2;
@@ -13,11 +14,7 @@ import com.github.visgeek.utils.testing.Assert2;
 public class EnumerableLinkedListTest {
 	@Test
 	public void ctorNoArgs() {
-		EnumerableLinkedList<Integer> actual = new EnumerableLinkedList<>();
-		actual.add(1);
-		actual.add(2);
-		actual.add(3);
-
+		EnumerableLinkedList<Integer> actual = new EnumerableLinkedList<>(1, 2, 3);
 		Assert2.assertSequanceEquals(actual, 1, 2, 3);
 	}
 
@@ -30,8 +27,16 @@ public class EnumerableLinkedListTest {
 	}
 
 	@Test
-	public void ctorIterable() {
+	public void ctorIterable_Collection() {
 		Iterable<Integer> values = Arrays.asList(1, 2, 3);
+		EnumerableLinkedList<Integer> actual = new EnumerableLinkedList<>(values);
+
+		Assert2.assertSequanceEquals(actual, 1, 2, 3);
+	}
+
+	@Test
+	public void ctorIterable_Iterable() {
+		Iterable<Integer> values = () -> Arrays.asList(1, 2, 3).iterator();
 		EnumerableLinkedList<Integer> actual = new EnumerableLinkedList<>(values);
 
 		Assert2.assertSequanceEquals(actual, 1, 2, 3);
@@ -43,65 +48,6 @@ public class EnumerableLinkedListTest {
 		EnumerableLinkedList<Integer> actual = new EnumerableLinkedList<>(values);
 
 		Assert2.assertSequanceEquals(actual, 1, 2, 3);
-	}
-
-	@Test
-	public void addAll01() {
-		Collection<Integer> valuesCollectionCollection = Arrays.asList(1, 2, 3);
-		Iterable<Integer> valuesIterableCollection = valuesCollectionCollection;
-		Iterable<Integer> valuesIterableIterable = () -> valuesCollectionCollection.iterator();
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		EnumerableLinkedList<Integer> list2 = new EnumerableLinkedList<>();
-		EnumerableLinkedList<Integer> list3 = new EnumerableLinkedList<>();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.addAll(valuesCollectionCollection);
-			boolean result2 = list2.addAll(valuesIterableCollection);
-			boolean result3 = list3.addAll(valuesIterableIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert.assertEquals(message, result1, result3);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
-	}
-
-	@Test
-	public void addAll02() {
-		Collection<Integer> valuesCollectionCollection = Arrays.asList(2, 3, 4);
-		Iterable<Integer> valuesIterableCollection = valuesCollectionCollection;
-		Iterable<Integer> valuesIterableIterable = () -> valuesCollectionCollection.iterator();
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>(1, 5);
-		EnumerableLinkedList<Integer> list2 = list1.clone();
-		EnumerableLinkedList<Integer> list3 = list1.clone();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.addAll(2, valuesCollectionCollection);
-			boolean result2 = list2.addAll(2, valuesIterableCollection);
-			boolean result3 = list3.addAll(2, valuesIterableIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert.assertEquals(message, result1, result3);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
-	}
-
-	@Test
-	public void any() {
-		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>();
-		Assert.assertFalse(list.any());
-
-		list.add(1);
-		Assert.assertTrue(list.any());
 	}
 
 	@Test
@@ -123,178 +69,69 @@ public class EnumerableLinkedListTest {
 	@Test
 	@SuppressWarnings("deprecation")
 	public void contains() {
-		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3);
 		Assert.assertTrue(list.contains(1));
 		Assert.assertFalse(list.contains(4));
-		Assert.assertTrue(list.containsValue(1));
-		Assert.assertFalse(list.containsValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void indexOf() {
-		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(2);
-
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3, 2);
 		Assert.assertEquals(1, list.indexOf(2));
 		Assert.assertEquals(-1, list.indexOf(4));
-		Assert.assertEquals(list.indexOf(2), list.indexOfValue(2));
-		Assert.assertEquals(list.indexOf(4), list.indexOfValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void lastIndexOf() {
-		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(2);
-
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3, 2);
 		Assert.assertEquals(3, list.lastIndexOf(2));
 		Assert.assertEquals(-1, list.lastIndexOf(4));
-		Assert.assertEquals(list.lastIndexOf(2), list.lastIndexOfValue(2));
-		Assert.assertEquals(list.lastIndexOf(4), list.lastIndexOfValue(4));
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
-	public void removeIndex_removeAt() {
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		list1.add(1);
-		list1.add(2);
-		list1.add(3);
+	public void removeIndex() {
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3);
 
-		EnumerableLinkedList<Integer> list2 = list1.clone();
+		{
+			int index = 1;
+			int expected = 2;
+			Integer actual = list.remove(index);
+			Assert.assertEquals(expected, (int) actual);
+		}
 
-		int index = 1;
-		Integer result1 = list1.remove(index);
-		Integer result2 = list2.removeAt(index);
-
-		Assert.assertEquals(Integer.valueOf(2), result1);
-		Assert.assertEquals(result1, result2);
-		Assert2.assertSequanceEquals(list1, list2);
-	}
-
-	@Test
-	@SuppressWarnings("deprecation")
-	public void removeObject_removeValue() {
-		int target = 2;
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		list1.add(target - 1);
-		list1.add(target);
-		list1.add(target + 1);
-		list1.add(target);
-
-		EnumerableLinkedList<Integer> list2 = list1.clone();
-
-		// target をすべて消して更にもう一回試みる。
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.remove(Integer.valueOf(target));
-			boolean result2 = list2.removeValue(target);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result1, result2);
-			Assert2.assertSequanceEquals(message, list1, list2);
+		{
+			int index = 0;
+			int expected = 1;
+			Integer actual = list.remove(index);
+			Assert.assertEquals(expected, (int) actual);
 		}
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
-	public void removeFirstOccurrence() {
-		int target = 2;
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		list1.add(target - 1);
-		list1.add(target);
-		list1.add(target + 1);
-		list1.add(target);
-
-		EnumerableLinkedList<Integer> list2 = list1.clone();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.removeFirstOccurrence(target);
-			boolean result2 = list2.removeFirstOccurredValue(target);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result2, result1);
-			Assert2.assertSequanceEquals(message, list2, list1);
-		}
-	}
-
-	@Test
-	@SuppressWarnings("deprecation")
-	public void removeLastOccurrence() {
-		int target = 2;
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		list1.add(target - 1);
-		list1.add(target);
-		list1.add(target + 1);
-		list1.add(target);
-
-		EnumerableLinkedList<Integer> list2 = list1.clone();
-
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.removeLastOccurrence(target);
-			boolean result2 = list2.removeLastOccurredValue(target);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result2, result1);
-			Assert2.assertSequanceEquals(message, list2, list1);
-		}
+	public void removeObject() {
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3);
+		Integer target = 2;
+		boolean actual = list.remove(target);
+		Assert.assertTrue(actual);
+		Assert2.assertSequanceEquals(list, 1, 3);
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
 	public void removeAllCollection() {
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3, 2, 4);
 		Collection<Integer> targetCollection = Arrays.asList(2, 3);
-		Iterable<Integer> targetIterable = () -> targetCollection.iterator();
-
-		EnumerableLinkedList<Integer> list1 = new EnumerableLinkedList<>();
-		list1.add(1);
-		list1.add(2);
-		list1.add(3);
-		list1.add(2);
-		list1.add(4);
-
-		EnumerableLinkedList<Integer> list2 = list1.clone();
-		EnumerableLinkedList<Integer> list3 = list1.clone();
-
-		// target をすべて消して更にもう一回試みる。
-		for (int i = 1; i <= 3; i++) {
-			boolean result1 = list1.removeAll(targetCollection);
-			boolean result2 = list2.removeAllValues(targetCollection);
-			boolean result3 = list3.removeAllValues(targetIterable);
-
-			String message = String.format("%d回目", i);
-
-			Assert.assertEquals(message, result2, result1);
-			Assert.assertEquals(message, result3, result1);
-
-			Assert2.assertSequanceEquals(message, list2, list1);
-			Assert2.assertSequanceEquals(message, list3, list1);
-		}
+		list.removeAll(targetCollection);
+		Assert2.assertSequanceEquals(list, 1, 4);
 	}
 
 	@Test
 	public void toString01() {
-		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>();
-		list.add(1);
-		list.add(2);
-		list.add(3);
-
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3);
 		Assert.assertEquals("size:3", list.toString());
 	}
 
@@ -314,5 +151,77 @@ public class EnumerableLinkedListTest {
 	public void getEmptyReadOnlyList() {
 		IReadOnlyList<Integer> list = EnumerableLinkedList.getEmptyReadOnlyList();
 		Assert.assertTrue(list.isEmpty());
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void removeFirstOccurrence() {
+		this.removeFirstOccurrenceInternal((list, target) -> list.removeFirstOccurrence(target));
+	}
+
+	@Test
+	public void removeFirstOccurredValue() {
+		this.removeFirstOccurrenceInternal((list, target) -> list.removeFirstOccurredValue(target));
+	}
+
+	private void removeFirstOccurrenceInternal(Func2<EnumerableLinkedList<Integer>, Integer, Boolean> method) {
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3, 2);
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertTrue(result);
+			Assert2.assertSequanceEquals(list, 1, 3, 2);
+		}
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertTrue(result);
+			Assert2.assertSequanceEquals(list, 1, 3);
+		}
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertFalse(result);
+			Assert2.assertSequanceEquals(list, 1, 3);
+		}
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void removeLastOccurrence() {
+		this.removeLastOccurrenceInternal((list, target) -> list.removeLastOccurrence(target));
+	}
+
+	@Test
+	public void removeLastOccurredValue() {
+		this.removeLastOccurrenceInternal((list, target) -> list.removeLastOccurredValue(target));
+	}
+
+	public void removeLastOccurrenceInternal(Func2<EnumerableLinkedList<Integer>, Integer, Boolean> method) {
+		EnumerableLinkedList<Integer> list = new EnumerableLinkedList<>(1, 2, 3, 2);
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertTrue(result);
+			Assert2.assertSequanceEquals(list, 1, 2, 3);
+		}
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertTrue(result);
+			Assert2.assertSequanceEquals(list, 1, 3);
+		}
+
+		{
+			Integer target = 2;
+			boolean result = method.func(list, target);
+			Assert.assertFalse(result);
+			Assert2.assertSequanceEquals(list, 1, 3);
+		}
 	}
 }
